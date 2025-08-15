04890f59d713be06831a3761627348c2fad458d0
Masunori <minhducduong128@gmail.com>
2025-08-15 18:58:57 +0800

    Update dummy user object for post-auth

--- client/src/app/(withContext)/home/page.tsx
+++ client/src/app/(withContext)/home/page.tsx
@@ -1,18 +1,19 @@
-import { User } from "@/app/userPrefs/userPrefsUtils"
-import { getProfile } from "@/lib/apiClient/user";
-import { redirect } from "next/navigation";
+import { PRISTINE_USER, User } from "@/app/userPrefs/userPrefsUtils"
+// import { getProfile } from "@/lib/apiClient/user";
+// import { redirect } from "next/navigation";  
 import HomeClient from "./HomeClient";
 
 export default async function Page() {
     // const user = useUserStore(state => state.user);
-    const response = await getProfile();
-    console.log(response);
+    // const response = await getProfile();
+    // console.log(response);
 
-    if (response.status === 401) {
-        redirect("/portal");
-    }
+    // if (response.status === 401) {
+    //     redirect("/portal");
+    // }
     
     return (
-        <HomeClient user={response.data as User} />
+        // <HomeClient user={response.data as User} />
+        <HomeClient user={PRISTINE_USER as User} />
     )
 }
