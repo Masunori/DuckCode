31fd212274cdc266a165a8f12647231fae605d08
Masunori <minhducduong128@gmail.com>
2026-04-10 11:21:37 +0800

    Disable /dump

--- client/src/app/dump/page.tsx
+++ client/src/app/dump/page.tsx
@@ -1,34 +1,35 @@
 "use client";
 
-import styles from "./dump.module.css";
-import rankStyles from "@/components/styles/ranks.module.css";
+// import styles from "./dump.module.css";
+// import rankStyles from "@/components/styles/ranks.module.css";
 
-import OTPInput from "@/components/inputs/OTPInput";
+// import OTPInput from "@/components/inputs/OTPInput";
 
 export default function DumpPage() {
-    const toHome = () => {
-        window.location.href = "/home";
-    }
+    return null;
+    // const toHome = () => {
+    //     window.location.href = "/home";
+    // }
 
-    return (
-        <div style={{
-            width: "500px",
-            marginTop: "50%",
-            marginLeft: "50%",
-            transform: "translate(-50%, -50%)"
-        }}>
-            <p>This is a dump page for debugging purposes.</p>
-            <OTPInput n={6} onOtpChange={otp => console.log(`OTP changed: ${otp}`)} onOtpValidate={isValid => console.log(`OTP valid: ${isValid}`)} />
-            <ul>
-                <li className={`${styles.rank} ${rankStyles.duckling}`}>DUCKLING</li>
-                <li className={`${styles.rank} ${rankStyles.mallard}`}>MALLARD</li>
-                <li className={`${styles.rank} ${rankStyles.teal}`}>TEAL</li>
-                <li className={`${styles.rank} ${rankStyles.swan}`}>SWAN</li>
-                <li className={`${styles.rank} ${rankStyles.grebe}`}>GREBE</li>
-                <li className={`${styles.rank} ${rankStyles.loon}`}>LOON</li>
-                <li className={`${styles.rank} ${rankStyles.phoenix}`}>PHOENIX</li>
-            </ul>
-            <button onClick={toHome}>To Home</button>
-        </div>
-    );
+    // return (
+    //     <div style={{
+    //         width: "500px",
+    //         marginTop: "50%",
+    //         marginLeft: "50%",
+    //         transform: "translate(-50%, -50%)"
+    //     }}>
+    //         <p>This is a dump page for debugging purposes.</p>
+    //         <OTPInput n={6} onOtpChange={otp => console.log(`OTP changed: ${otp}`)} onOtpValidate={isValid => console.log(`OTP valid: ${isValid}`)} />
+    //         <ul>
+    //             <li className={`${styles.rank} ${rankStyles.duckling}`}>DUCKLING</li>
+    //             <li className={`${styles.rank} ${rankStyles.mallard}`}>MALLARD</li>
+    //             <li className={`${styles.rank} ${rankStyles.teal}`}>TEAL</li>
+    //             <li className={`${styles.rank} ${rankStyles.swan}`}>SWAN</li>
+    //             <li className={`${styles.rank} ${rankStyles.grebe}`}>GREBE</li>
+    //             <li className={`${styles.rank} ${rankStyles.loon}`}>LOON</li>
+    //             <li className={`${styles.rank} ${rankStyles.phoenix}`}>PHOENIX</li>
+    //         </ul>
+    //         <button onClick={toHome}>To Home</button>
+    //     </div>
+    // );
 }
